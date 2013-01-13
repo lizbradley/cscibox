243c4199226869223b91721fda450ad874c42422
Jada Ballentine <jada.ballentine@colorado.edu>
2013-01-13 10:12:34 -0700

    Just put the scipy.interp function.  Doesn't do anything yet, just wanted to make sure this was going through

--- src/cscience/components/__init__.py
+++ src/cscience/components/__init__.py
@@ -17,6 +17,11 @@ class BaseComponent(object):
         self.computation_plan = experiment
 
     def __call__(self, samples):
+        
+        from scipy.interpolate import griddata
+        #grid_z0 = griddata(points, values, (grid_x, grid_y), method='nearest')
+        #grid_z1 = griddata(points, values, (grid_x, grid_y), method='linear')
+        #grid_z2 = griddata(points, values, (grid_x, grid_y), method='cubic')
         """
         Method that does all the actual work
         """
