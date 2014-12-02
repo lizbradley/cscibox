43cc0404a4d3b182467bb36ff498e621ed391a17
Fernando Nobre <fernobre@gmail.com>
2014-12-02 12:49:12 -0700

    changed 'Ice Core Model Age' field to 'Flow Model Age'

--- src/cscience/components/flowmodels.py
+++ src/cscience/components/flowmodels.py
@@ -7,7 +7,7 @@ class DansgaardJohnsen(cscience.components.BaseComponent):
 
     visible_name = 'Dansgaard-Johnsen ice core flow model'
     inputs = {'required':('depth',)}
-    outputs = {'Ice Core Model Age': ('float', 'kyears', True)}
+    outputs = {'Flow Model Age': ('float', 'kyears', True)}
 
     def run_component(self, core):
 
@@ -30,7 +30,7 @@ class DansgaardJohnsen(cscience.components.BaseComponent):
             elif z >= 0  and z < h:
                 age = ((2*H - h)/(2*c)) * np.log((2*H - h)/(2*z - h))
 
-            sample['Ice Core Model Age'] = UncertainQuantity(age, 'years',
+            sample['Flow Model Age'] = UncertainQuantity(age, 'years',
                                 0)
 
 
