d3c1949c81196ca238b5fbe4aac0edf4331853ec
Fernando Nobre <fernobre@gmail.com>
2014-12-05 19:49:26 -0700

    correction to the flow model so it will list ages increasing as the depth increses

--- src/cscience/components/flowmodels.py
+++ src/cscience/components/flowmodels.py
@@ -2,6 +2,7 @@ import cscience.components
 import calvin.argue
 import numpy as np
 from cscience.components import UncertainQuantity
+import pdb
 
 class DansgaardJohnsen(cscience.components.BaseComponent):
 
@@ -15,6 +16,8 @@ class DansgaardJohnsen(cscience.components.BaseComponent):
         H = parameters['Ice Thickness(m)']
         h = parameters['Linear Depth(m)']
         c = parameters['Accumulation Rate(m/yr)']
+
+        samples = []
 
         for sample in core:
             # Converto depth to meters
@@ -31,7 +34,25 @@ class DansgaardJohnsen(cscience.components.BaseComponent):
                 age = ((2*H - h)/(2*c)) * np.log((2*H - h)/(2*z - h))
 
             sample['Flow Model Age'] = UncertainQuantity(age, 'years',
-                                0)
+                               0)
+            samples.append(sample)
+
+        # Sort the samples
+        samples.sort(key=lambda x: x["depth"])
+
+        # Invert the ages
+        for ii in range (0, len(samples)/2):
+            end_index = len(samples) - 1 - ii
+            if (end_index <= ii):
+                break
+
+            temp = samples[ii]["Flow Model Age"]
+            samples[ii]["Flow Model Age"] = samples[end_index]['Flow Model Age']
+            samples[end_index]['Flow Model Age'] = temp
+
+
+
+
 
 
 
