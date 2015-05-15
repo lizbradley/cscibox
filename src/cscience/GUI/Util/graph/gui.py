45f1fb089e260b9f088d46094f257388a99dca84
Joshua Rahm <joshua.rahm@colorado.edu>
2015-05-15 12:53:11 -0600

    simple border

--- src/cscience/GUI/Util/graph/gui.py
+++ src/cscience/GUI/Util/graph/gui.py
@@ -354,7 +354,7 @@ class StylePanel(wx.Panel):
     
     def transient_bind_closure(self):
         win = TestTransientPopup(self, wx.SIMPLE_BORDER)
-        panel = CalCheckboxPanel( zip(self._m_computation_plans, self._m_computation_plans), win, style=wx.SUNKEN_BORDER)
+        panel = CalCheckboxPanel( zip(self._m_computation_plans, self._m_computation_plans), win, style=wx.SIMPLE_BORDER)
         win.set_panel(panel)
         
         def return_function(evt):
