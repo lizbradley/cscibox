887f28c512ad2c2ccae18dea1febd6f32ebb7bb0
Laura de Vesine <ldevesine@ziz.org>
2015-09-30 15:18:53 -0600

    add aktual data!

--- src/ai_testing.py
+++ src/ai_testing.py
@@ -12,6 +12,11 @@ if __name__ == '__main__':
 
         raise datastore.RepositoryException()
     
-    env = environment.Environment(datastore.cores['NGRIP - Ice Core'])
+    core = datastore.cores['NGRIP - Ice Core']
+    core['all']['current average temperature'] = -3.34
+    core['all']['current temperature variability'] = 482.9
+    core['all']['latitude'] = 75.1
+    core['all']['longitude'] = -42.32
+    env = environment.Environment(core)
     result = engine.build_argument(conclusions.Conclusion('no snow melt'), env)
     print result
