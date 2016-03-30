ee7d88b886f2e1df746d3ca7bf7a4c024f7664ca
rishabhy <yadav.g.rishabh@gmail.com>
2016-03-30 14:20:39 -0600

    Various interpolations added as components

--- src/cscience/components/interpolations.py
+++ src/cscience/components/interpolations.py
@@ -1,6 +1,7 @@
 import cscience.components
 import numpy as np
 import scipy.interpolate
+import scipy.stats
 from cscience.components import UncertainQuantity
 
 class PointlistInterpolation(object):
@@ -44,6 +45,42 @@ class InterpolateModelSpline(cscience.components.BaseComponent):
         x_i, y_i = scipy.interpolate.splev(np.linspace(0, 1, 100), tck)
         core['all']['age/depth model'] = PointlistInterpolation(x_i, y_i)
 
+class InterpolateModelRegression(cscience.components.BaseComponent):
+    visible_name = 'Interpolate Age/Depth Model (Linear Regression)'
+    inputs = {'required':('Calibrated 14C Age',)}
+
+    def run_component(self, core):
+        x = [sample['depth'].item() for sample in core]
+        y = [sample['Calibrated 14C Age'] for sample in core]
+        slope, y_intcpt, r_value, p_value, std_err = scipy.stats.linregress(x, y)
+        core['all']['age/depth model'] = PointlistInterpolation([i for i in x], [y_intcpt + slope * i for i in x])
+
+class InterpolateModelCubic(cscience.components.BaseComponent):
+    visible_name = 'Interpolate Age/Depth Model (Cubic)'
+    inputs = {'required':('Calibrated 14C Age',)}
+    outputs = {'PointlistInterpolation'}
+
+    def run_component(self, core):
+        x = [sample['depth'] for sample in core]
+        y = [sample['Calibrated 14C Age'] for sample in core]
+        interp_func = scipy.interpolate.interp1d([float(i) for i in x], [float(i) for i in y],
+                               bounds_error=False, fill_value=0, kind='cubic')
+        new_x = np.arange(min(x), max(x), abs(max(x)-min(x))/100.0)
+        core['all']['age/depth model'] = PointlistInterpolation(new_x, interp_func(new_x)) 
+
+class InterpolateModelQuadratic(cscience.components.BaseComponent):
+    visible_name = 'Interpolate Age/Depth Model (Quadratic)'
+    inputs = {'required':('Calibrated 14C Age',)}
+    #outputs = {} #TODO: add an output type that is an object, for great justice
+
+    def run_component(self, core):
+        x = [sample['depth'] for sample in core]
+        y = [sample['Calibrated 14C Age'] for sample in core]
+        interp_func = scipy.interpolate.interp1d([float(i) for i in x], [float(i) for i in y],
+                               bounds_error=False, fill_value=0, kind='quadratic')
+        new_x = np.arange(min(x), max(x), abs(max(x)-min(x))/100.0)
+        core['all']['age/depth model'] = PointlistInterpolation(new_x, interp_func(new_x)) 
+
 class UseModel(cscience.components.BaseComponent):
 
     visible_name = 'Assign Ages Using Age-Depth Model'
@@ -55,8 +92,6 @@ class UseModel(cscience.components.BaseComponent):
         #been interpolated using some method, and is now associating ages
         #based on that model with all points along the depth curve.
         age_model = core['all']['age/depth model']
-        print age_model
         for sample in core:
             sample['Model Age'] = age_model(sample['depth'])
-
         
