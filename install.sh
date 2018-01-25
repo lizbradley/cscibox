766bc918fb5f9c46d33a9ab5264085674d0de4cc
Suyog Soti <suyog.soti@gmail.com>
2018-01-25 09:12:44 -0700

    tweak docker install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@
 
 docker build -t cscibox .
 docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY --device /dev/snd --name cscibox cscibox
+
+# To get the repository image so you don't have to download the repository from github
+# docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY --device /dev/snd --name cscibox suyogsoti/cscibox
